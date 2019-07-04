a7295d91c3803afbb557d96a7870117b2187360d
iamhridoy11 <m_rahman_hridoy@yahoo.com>
2019-07-04 13:43:30 -0400

    Initial commit

---
+++ app/src/main/java/ie/cm/activities/Help.java
@@ -0,0 +1,14 @@
+package ie.cm.activities;
+
+import android.os.Bundle;
+
+import ie.cm.R;
+
+public class Help extends Base {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
